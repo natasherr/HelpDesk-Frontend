645c4e65148ae7fdd243b538f4e7e2bce83a4255
Ashley Natasha <ashleyotsiula@gmail.com>
2025-02-18 13:02:03 +0300

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,33 @@
+import React from 'react'
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Categories from "./pages/Categories"
+import Profile from "./pages/Profile"
+import Register from "./pages/Register"
+import About from "./pages/About"
+import Problems from "./pages/Problems"
+import Faqs from "./pages/Faqs"
+import Layout from './components/Layout'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+          
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="categories" element={<Categories />} />
+                <Route path="problems" element={<Problems />} />
+                <Route path="about" element={<About />} />
+                <Route path="faqs" element={<Faqs />} />
+                {/* <Route path="*" element={<NoPage />} /> */}
+              </Route>
+            </Routes>
+
+    </BrowserRouter>
   )
 }
---
+++ frontend/src/pages/Faqs.jsx
@@ -0,0 +1,10 @@
+
+const faqs = () => {
+  return (
+    <div>
+    <h1>FAQ</h1>
+    </div>
+  )
+};
+
+export default faqs
