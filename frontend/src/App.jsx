3ff3e66d63a16f299d23b1e04e2a2aabc8bbcdd4
Ashley Natasha <ashleyotsiula@gmail.com>
2025-02-21 13:38:20 +0300

    Context and NoPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,41 @@
+import React from 'react'
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Categories from "./pages/Categories"
+import Profile from "./pages/Profile"
+import Register from "./pages/Register"
+import About from "./pages/About"
+import Problems from "./pages/Problems"
+import Faqs from "./pages/Faqs"
+import Layout from './components/Layout'
+import NoPage from './pages/NoPage'
+import { UserProvider } from './context/UserContext'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <UserProvider>
+
+            <Routes>
+
+              <Route>
+                  <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="profile" element={<Profile />} />
+                    <Route path="categories" element={<Categories />} />
+                    <Route path="problems" element={<Problems />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="faqs" element={<Faqs />} />
+                    <Route path="*" element={<NoPage />} />
+                  </Route>
+              </Route>
+              
+            </Routes>
+
+      </UserProvider>
+    </BrowserRouter>
   )
 }
